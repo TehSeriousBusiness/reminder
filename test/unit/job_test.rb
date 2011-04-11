ff789ca71a003e26256c264af65b2fc1a955f2d1
dimitrij <dimitrij.heid@epos-cat.de>
2011-04-11 11:05:59 +0200

    changed test/unit/job_test.rb added some variables to use in the tests for a missing parameter
    Signed-off-by: dimitrij <dimitrij.heid@epos-cat.de>

--- test/unit/job_test.rb
+++ test/unit/job_test.rb
@@ -1,8 +1,36 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  #### Missing Parameters START ####
+  ### Checking for missing parameters
+  ##  t.string :initiator
+  ##  t.text :destinations
+  ##  t.string :subject
+  ##  t.text :content
+  ##  t.binary :attachement
+  ##  t.date :kickOff
+  ##  t.integer :repetition
+  ##  t.integer :delay
+  ### init predefined parameters to use in all "missing paramteres tests"
+  testVarInitiator = "myUserName"
+  testVarDestinations = "dest1@host1.de;dest2@host2.com;dest3@host3.org"
+  testVarSubject = "This is a Subject"
+  testVarContent = "This is a very long Text, This is also the Content of the Email"
+  testVarAttachment = "asdfasf" # iam a binary TODO CHANGE ME!
+  testVarKickOff = "myUserName"
+  testVarDelay = "myUserName"
+  
+  test "Missing Parameters - initiator" do
+    # No initiator is set
+    job = Job.new(:username => "Jarqueline")
+    assert !(job.save()), 'password required'
   end
+  #### Missing Parameters END ####
+  
+  
+  #### XYZ START ####
+  
+  #### XYZ END ####
+  
 end
