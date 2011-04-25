d579d32d4f25769ea61c92af75bd1857eb987a4c
maven <m@v.en>
2011-04-25 14:32:34 +0200

    job_template implemented 25 tests, 27 assertions, 0 failures, 0 errors, 0 skips (inclusive user tests)

--- app/models/job_template.rb
+++ app/models/job_template.rb
@@ -1,2 +1,17 @@
 class JobTemplate < ActiveRecord::Base
+  
+  # Adding templateName to the attr_accessor list cause failure.
+  attr_accessor :templateSubject, :templateContent
+  
+  # Urgently needed!
+  validates_presence_of :templateName
+  
+  # TemplateName is one of a kind.
+  validates_uniqueness_of :templateName
+  
+  # Every member variable got a maximum length of characters.
+  validates_length_of :templateName,    :maximum =>  40
+  validates_length_of :templateSubject, :maximum =>  40
+  validates_length_of :templateContent, :maximum => 250
+ 
 end
