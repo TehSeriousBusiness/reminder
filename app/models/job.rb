0fbeb479b44345bbfdb8f388c071e32648365cb0
kater <kater@nettopLX>
2011-05-13 15:19:13 +0200

    validator email geht vllt nun =D

--- app/models/job.rb
+++ app/models/job.rb
@@ -39,12 +39,17 @@ class Job < ActiveRecord::Base
   # validates_format_of :destinations, :with => "(/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i;)+"
   #validates_format_of :destinations, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,};)\Z/i
   validate :email => true
+
+  
   def email
-    emailArray = :destinations.split(";")
+    emailArray = :destinations.split(/;+/)
     emailArray.each do |email|
-      puts "test"
-#      if(!/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.match(email))
-        if(!/\Aasdf\Z/i.match(email))
+      #      puts "email:" + email +"!END"
+      #      if(email == nil)
+      #        puts "nil email found"
+      #      end
+      if(!/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.match(email))
+        #        puts "invalid " + email
         record.errors[attribute] << "destination " + email + " is wrong"
       end
     end
