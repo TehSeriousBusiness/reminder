25609f0b03b924916631174cf61951679ca8e58e
dimitrij <dimitrij.heid@epos-cat.de>
2011-05-23 11:26:07 +0200

    test commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,11 +11,11 @@ set :branch, "devD_Capistrano"
 set :deploy_via, :remote_cache
 
 
+set :domain, "kater.homelinux.org"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "#{domain}"                          # Your HTTP server, Apache/etc
+role :app, "#{domain}"                          # This may be the same as your `Web` server
+role :db,  "#{domain}", :primary => true # This is where Rails migrations will run
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
@@ -28,3 +28,11 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+
+
+#### USING THIS GUIDE https://github.com/capistrano/capistrano/wiki/2.x-From-The-Beginning
+set :deploy_to, "/home/sweng/www"
+set :user, "sweng"
+set :use_sudo, false
+
