3ba4b61eee4863f58d7e2da1e04007ffff5b67e8
maven <m@v.en>
2011-06-08 22:33:35 +0200

    ignore user mailer folder, redirection setting

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -18,7 +18,7 @@ class UserMailer < ActionMailer::Base
   # setup
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
-    message.to = "teh.serious.business@gmx.net"
+    message.to = "manuel.hager@gmx.de"
   end
   
   # teardown
