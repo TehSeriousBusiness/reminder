2a16416302f58a710f7e566f76a337924010b41c
maven <m@v.en>
2011-06-23 00:29:47 +0200

    Bugfix

--- app/mailers/background.rb
+++ app/mailers/background.rb
@@ -3,9 +3,11 @@ class Background
 	
 	def sendLater(job)
 		@job = job
+		
+		currentTime = Time.now
 	
 		#call UserMailer.send_mail(@job)
-		if( @job.kickOff.utc <= Time.now.utc)
+		if( @job.kickOff <= currentTime)
 			begin
 				UserMailer.send_mail(@job)
 			rescue Exception => e
@@ -21,11 +23,16 @@ class Background
 			elsif (@job.repetition != 0)
 				@job.repetition -= 1
 			end
-			@job.kickOff = Time.now.utc + @job.delay.seconds
+			@job.kickOff = currentTime + @job.delay.seconds
 			@job.save()
+			
+			#send later
+			puts "Now! Next mail in #{@job.delay} sec!"
+			Background.instance.delay({:run_at => @job.delay.from_now}).sendLater(@job)
+		else	
+			waitingTime = @job.kickOff.to_i - currentTime.to_i
+			puts "Send mail in #{waitingTime} sec!"
+			Background.instance.delay({:run_at => waitingTime.from_now}).sendLater(@job)
 		end
-		
-		#send later
-		Background.instance.delay({:run_at => @job.delay.from_now}).sendLater(@job)
 	end
 end
